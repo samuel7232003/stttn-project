2779f0f3bc71c854f40d4748bbdfb3d11fe28953
Le Viet Thanh <115569080+samuel7232003@users.noreply.github.com>
2025-02-27 00:35:51 +0700

    [updaet]

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import { useState } from "react";
+import "./home.css"
 
 export default function Home(){
     const [curPage, setCurPage] = useState("home");
     
     return (
-        <div>
+        <div className="home-main">
             <Header curPage={curPage}/>
             <div style={{paddingTop: "70px"}}>
                 <Outlet context={{setCurPage}}/>
